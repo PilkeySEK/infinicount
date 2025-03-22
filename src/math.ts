6a8ea9e9e0edbe51ed1145676a315f0441ceadb8
PilkeySEK <PilkeySEK@gmx.de>
2025-03-22 10:49:42 +0100

    format

--- src/math.ts
+++ src/math.ts
@@ -1,47 +1,27 @@
-import { Complex, evaluate, isComplex} from "mathjs";
+import { Complex, evaluate, isComplex } from "mathjs";
 
 // If a message matches these EXACTLY, don't count it
-const exclusions = [
-    ":3",
-    "s",
-    "br",
-    "e",
-    "g",
-    ";3",
-    "{}",
-    "[]",
-    "V",
-    "S",
-    "E",
-    "F",
-    "J",
-    "K",
-    "L",
-    "N",
-    "W",
-    "T",
-    
-]
+const exclusions = [":3", "s", "br", "e", "g", ";3", "{}", "[]", "V", "S", "E", "F", "J", "K", "L", "N", "W", "T"]
 
 export function evalMessage(content: string): number | Complex | undefined {
-    if(content.startsWith('`') && content.endsWith('`')) {
+    if (content.startsWith('`') && content.endsWith('`')) {
         content = content.substring(1, content.length - 1);
     }
     let math_res: number | Complex | undefined;
     try {
         math_res = evaluate(content);
     }
-    catch(e) {
+    catch (e) {
         math_res = undefined;
     }
     const math_str = math_res == undefined ? "" : math_res.toString();
-    if(isComplex(math_res)) {
-        if(!math_str.includes("i")) {
+    if (isComplex(math_res)) {
+        if (!math_str.includes("i")) {
             console.log(`Converted Complex Number ${math_res} to number.`);
             math_res = parseInt(math_res.toString());
         }
     }
-    if(exclusions.includes(math_str)) return undefined;
+    if (exclusions.includes(math_str)) return undefined;
     console.log(`Result of "${content}": ${math_res}`);
     return math_res;
 }
