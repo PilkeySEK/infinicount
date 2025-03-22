ac753b320621c71579eeabf516d4893a37c09956
PilkeySEK <PilkeySEK@gmx.de>
2025-03-22 10:43:42 +0100

    Add exclusions

--- src/math.ts
+++ src/math.ts
@@ -1,5 +1,18 @@
 import { Complex, evaluate, isComplex} from "mathjs";
 
+// If a message matches these EXACTLY, don't count it
+const exclusions = [
+    ":3",
+    "s",
+    "br",
+    "e",
+    "g",
+    ";3",
+    "{}",
+    "[]",
+    "V"
+]
+
 export function evalMessage(content: string): number | Complex | undefined {
     if(content.startsWith('`') && content.endsWith('`')) {
         content = content.substring(1, content.length - 1);
@@ -11,13 +24,14 @@ export function evalMessage(content: string): number | Complex | undefined {
     catch(e) {
         math_res = undefined;
     }
+    const math_str = math_res == undefined ? "" : math_res.toString();
     if(isComplex(math_res)) {
-        const math_str = math_res.toString();
         if(!math_str.includes("i")) {
             console.log(`Converted Complex Number ${math_res} to number.`);
             math_res = parseInt(math_res.toString());
         }
     }
+    if(exclusions.includes(math_str)) return undefined;
     console.log(`Result of "${content}": ${math_res}`);
     return math_res;
 }
